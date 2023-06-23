fda5035c5e4d10b5e4670074806d712aebdf7647
VictoryJu <kob0805@naver.com>
2023-06-23 19:47:29 +0900

    feat: 공통 섹션 CMSection 컴포넌트 추가

---
+++ src/components/common/CMSection.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+};
+
+const CMSection = ({ children, title }: Props) => {
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      {children}
+    </S.Container>
+  );
+};
+
+const S = {
+  Container: styled.section`
+    background-color: #fff;
+    padding: 25px;
+    border-radius: 12px;
+  `,
+  Title: styled.div`
+    font-weight: bold;
+    font-size: 17px;
+  `,
+};
+
+export default CMSection;
